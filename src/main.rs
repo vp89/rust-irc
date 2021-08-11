4c7c4fa0b1d90add8aae1b2c7f4886ff45740eb4
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2021-08-10 21:30:10 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}, thread};
+
+fn main() -> io::Result<()> {
+    println!("STARTING SERVER ON 127.0.0.1:6667");
+
+    let listener = TcpListener::bind("127.0.0.1:6667")?;
+    let mut connection_handles = vec![];
+
+    for connection_attempt in listener.incoming() {
+        match connection_attempt {
+            Ok(stream) => {
+                connection_handles.push(
+                    thread::spawn(|| {
+                        let conn_outcome = handle_connection(stream);
+                        match conn_outcome {
+                            Ok(_) => (),
+                            Err(e) => println!("ERROR {}", e)
+                        }
+                    }));
+            },
+            Err(error) => println!("ERROR CONNECTING {}", error),
+        };
+    }
+
+    for handle in connection_handles {
+        handle.join().expect("TODO")
+    }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    loop {
+        let mut buffer = [0;512];
+        stream.read(&mut buffer)?;
+        let raw_payload = std::str::from_utf8(&buffer)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+        println!("RECEIVED {}", raw_payload);
+
+        let messages = raw_payload.lines();
+
+        println!("SPLIT INTO {} MESSAGES", messages.clone().count());
+
+        for message in messages {
+            if message.starts_with("NICK") {
+                println!("MESSAGE {}", message);
+
+                let nick = message.split_whitespace().skip(1).next().unwrap_or("");
+                let rplmsgs = format!(
+                    ":localhost 001 {nick} :WELCOME TO THE SERVER {nick}\r\n:localhost 002 {nick} :Your host is localhost, running version 0.0.1\r\n:localhost 003 {nick} :This server was created now\r\n:localhost 004 {nick} localhost 0.0.1 r i\r\n:localhost 005 {nick} CHANNELLEN=32 :are supported by this server\r\n:localhost 251 {nick} :There are 100 users and 20 invisible on 1 servers\r\n:localhost 252 {nick} 1337 :IRC Operators online\r\n:localhost 253 {nick} 7 :unknown connection(s)\r\n:localhost 254 {nick} 9999 :channels formed\r\n:localhost 255 {nick} :I have 900 clients and 1 servers\r\n:localhost 265 {nick} 845 1000 :Current local users 845, max 1000\r\n:localhost 266 {nick} 9823 23455 :Current global users 9823, max 23455\r\n:localhost 250 {nick} :Highest connection count: 9998 (9000 clients) (99999 connections received)\r\n:localhost 375 {nick} :- localhost Message of the Day - \r\n:localhost 372 {nick} :- Foobar\r\n:localhost 376 {nick} :End of /MOTD command.",
+                    nick = nick);
+
+                println!("SENDING {}", rplmsgs);
+                stream.write(rplmsgs.as_bytes())?;
+                stream.flush()?;
+            } else {
+                println!("MESSAGE UNHANDLED {}", message);
+            }
+        }
+    }
+}
---
+++ src/messages.rs
@@ -0,0 +1,19 @@
+// TODO write a module for the message parsing with unit tests
+
+/*
+
+struct IrcMessage {
+    prefix: IrcPrefix
+}
+
+struct IrcPrefix {
+    servername: String,
+    nick: String,
+    user: Option<String>,
+    host: Option<String>
+}
+
+struct IrcCommand {
+    
+}
+ */
