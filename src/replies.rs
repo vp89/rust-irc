148260f9024103894c64f8c197bd89655fa3bd3f
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2021-08-31 22:28:11 -0400

    add some replies needed for channeljoining

--- src/replies.rs
+++ src/replies.rs
@@ -15,7 +15,10 @@ pub enum Reply<'a> {
     LuserMe { host: &'a str, nick: &'a str, clients: u32, servers: u32 },
     LocalUsers { host: &'a str, nick: &'a str, current: u32, max: u32 },
     GlobalUsers { host: &'a str, nick: &'a str, current: u32, max: u32 },
+    EndOfWho { host: &'a str },
     Topic { host: &'a str, nick: &'a str, channel: &'a str, topic: &'a str },
+    ListEnd { host: &'a str },
+    EndOfNames { host: &'a str },
     Motd { host: &'a str, nick: &'a str, line: &'a str },
     MotdStart { host: &'a str, nick: &'a str },
     EndOfMotd { host: &'a str, nick: &'a str },
@@ -51,7 +54,10 @@ impl Display for Reply<'_> {
             Reply::GlobalUsers { host, nick, current, max } => {
                 write!(f, ":{} 266 {} {} {} :Current global users {}, max {}", host, nick, current, max, current, max)
             },
+            Reply::EndOfWho { host } => write!(f, ":{} 315 :End of /WHO list", host),
             Reply::Topic { host, nick, channel, topic } => write!(f, ":{} 322 {} {} :{}", host, nick, channel, topic),
+            Reply::ListEnd { host} => write!(f, ":{} 323 :End of /LIST", host),
+            Reply::EndOfNames { host } => write!(f, ":{} 366 :End of /NAMES list", host),
             Reply::Motd { host, nick, line } => write!(f, ":{} 372 {} :- {}", host, nick, line),
             Reply::MotdStart { host, nick } => write!(f, ":{} 375 {} :- {} Message of the Day -", host, nick, host),
             Reply::EndOfMotd { host, nick } => write!(f, ":{} 376 {} :End of /MOTD command.", host, nick),
@@ -197,5 +203,29 @@ fn pong_prints_correctly() {
     let reply = Reply::Pong { host: "localhost", token: "LAG1238948394".to_string() };
     let actual = reply.to_string();
     let expected = format!(":localhost PONG localhost :LAG1238948394");
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn listend_prints_correctly() {
+    let reply = Reply::ListEnd { host: "localhost" };
+    let actual = reply.to_string();
+    let expected = format!(":localhost 323 :End of /LIST");
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn endofnames_prints_correctly() {
+    let reply = Reply::EndOfNames { host: "localhost" };
+    let actual = reply.to_string();
+    let expected = format!(":localhost 366 :End of /NAMES list");
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn endofwho_prints_correctly() {
+    let reply = Reply::EndOfWho { host: "localhost" };
+    let actual = reply.to_string();
+    let expected = format!(":localhost 315 :End of /WHO list");
     assert_eq!(expected, actual);
 }
