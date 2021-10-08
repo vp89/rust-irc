6553675225d41e7eaaf3a90bd64a53b7a9b6d966
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2021-10-07 22:24:56 -0400

    handle errors on server reply loop

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::{Error, ErrorKind}, sync::{Arc, RwLock, mpsc::{Receiver}}};
+use std::{collections::HashMap, io::{Error, ErrorKind}, sync::{Arc, RwLock, mpsc::{Receiver, Sender}}};
 use chrono::Utc;
 use uuid::Uuid;
 
@@ -26,29 +26,43 @@ pub fn run_server(
         let ctx_nick = &conn_ctx.nick;
         let ctx_sender = conn_ctx.client_sender_channel.lock().unwrap().clone();
 
-        let result = match &received.command {
+        match &received.command {
             ClientToServerCommand::Join { channels} => {
                 let now = Utc::now();
 
                 for channel in channels {
-                    ctx_sender.send(Reply::Join { client: ctx_client.clone(), channel: channel.clone() });
-                    // TODO have Nick available here
-                    // TODO persist the channel metadata
-                    ctx_sender.send(Reply::Topic { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), topic: "foobar topic".to_string() });
-                    ctx_sender.send(Reply::TopicWhoTime { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone(), set_at: now.clone() });
-                    ctx_sender.send(Reply::NamReply { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone() });
+                    send_replies(
+                        &ctx_sender,
+                        vec![
+                            Reply::Join { client: ctx_client.clone(), channel: channel.clone() },
+                            // TODO have Nick available here
+                            // TODO persist the channel metadata
+                            Reply::Topic { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), topic: "foobar topic".to_string() },
+                            Reply::TopicWhoTime { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone(), set_at: now.clone() },
+                            Reply::NamReply { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone() }
+                        ]);
                 }
             },
             ClientToServerCommand::Mode { channel } => {
                 let now = Utc::now();
 
-                ctx_sender.send(Reply::Mode { host: host.clone(), channel: channel.clone(), mode_string: "+tn".to_string() });
-                ctx_sender.send(Reply::ChannelModeIs { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), mode_string: "+mtn1".to_string(), mode_arguments: "100".to_string() });
-                ctx_sender.send(Reply::CreationTime { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), created_at: now.clone() });
+                send_replies(
+                    &ctx_sender,
+                    vec![
+                        Reply::Mode { host: host.clone(), channel: channel.clone(), mode_string: "+tn".to_string() },
+                        Reply::ChannelModeIs { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), mode_string: "+mtn1".to_string(), mode_arguments: "100".to_string() },
+                        Reply::CreationTime { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone(), created_at: now.clone() }
+                    ]
+                )
             },
             ClientToServerCommand::Who { channel } => {
-                ctx_sender.send(Reply::WhoReply { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone(), other_nick: "~vince".to_string(), client: "localhost".to_string() });
-                ctx_sender.send(Reply::EndOfWho { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone() });
+                send_replies(
+                    &ctx_sender,
+                    vec![
+                        Reply::WhoReply { host: host.clone(), channel: channel.clone(), nick: ctx_nick.clone(), other_nick: "~vince".to_string(), client: "localhost".to_string() },
+                        Reply::EndOfWho { host: host.clone(), nick: ctx_nick.clone(), channel: channel.clone() }    
+                    ]
+                )
             },
             ClientToServerCommand::Nick { nick } => {
                 let mut conn_write = connections.write().unwrap();
@@ -56,24 +70,29 @@ pub fn run_server(
                 writable_ctx.nick = nick.to_string();
                 writable_ctx.client = format!("{}!~{}@localhost", nick, nick); 
 
-                ctx_sender.send(Reply::Welcome { host: host.clone(), nick: nick.clone() });
-                ctx_sender.send(Reply::YourHost { host: host.clone(), nick: nick.clone(), version: ctx_version.clone() });
-                ctx_sender.send(Reply::Created { host: host.clone(), nick: nick.clone(), created_at: ctx_created_at.clone() });
-                ctx_sender.send(Reply::MyInfo { host: host.clone(), nick: nick.clone(), version: ctx_version.clone(), user_modes: "r".to_string(), channel_modes: "i".to_string() });
-                ctx_sender.send(Reply::Support { host: host.clone(), nick: nick.clone(), channel_len: 32 });
-                ctx_sender.send(Reply::LuserClient { host: host.clone(), nick: nick.clone(), visible_users: 100, invisible_users: 20, servers: 1 });
-                ctx_sender.send(Reply::LuserOp { host: host.clone(), nick: nick.clone(), operators: 1337 });
-                ctx_sender.send(Reply::LuserUnknown { host: host.clone(), nick: nick.clone(), unknown: 7 });
-                ctx_sender.send(Reply::LuserChannels { host: host.clone(), nick: nick.clone(), channels: 9999 });
-                ctx_sender.send(Reply::LuserMe { host: host.clone(), nick: nick.clone(), clients: 900, servers: 1 });
-                ctx_sender.send(Reply::LocalUsers { host: host.clone(), nick: nick.clone(), current: 845, max: 1000 });
-                ctx_sender.send(Reply::GlobalUsers { host: host.clone(), nick: nick.clone(), current: 9832, max: 23455 });
-                ctx_sender.send(Reply::StatsDLine { host: host.clone(), nick: nick.clone(), connections: 9998, clients: 9000, received: 99999 });
-
-                ctx_sender.send(Reply::MotdStart { host: host.clone(), nick: nick.clone() });
-                // TODO proper configurable MOTD
-                ctx_sender.send(Reply::Motd { host: host.clone(), nick: nick.clone(), line: "Foobar".to_string() });
-                ctx_sender.send(Reply::EndOfMotd { host: host.clone(), nick: nick.clone() });
+                send_replies(
+                    &ctx_sender,
+                    vec![
+                        Reply::Welcome { host: host.clone(), nick: nick.clone() },
+                        Reply::YourHost { host: host.clone(), nick: nick.clone(), version: ctx_version.clone() },
+                        Reply::Created { host: host.clone(), nick: nick.clone(), created_at: ctx_created_at.clone() },
+                        Reply::MyInfo { host: host.clone(), nick: nick.clone(), version: ctx_version.clone(), user_modes: "r".to_string(), channel_modes: "i".to_string() },
+                        Reply::Support { host: host.clone(), nick: nick.clone(), channel_len: 32 },
+                        Reply::LuserClient { host: host.clone(), nick: nick.clone(), visible_users: 100, invisible_users: 20, servers: 1 },
+                        Reply::LuserOp { host: host.clone(), nick: nick.clone(), operators: 1337 },
+                        Reply::LuserUnknown { host: host.clone(), nick: nick.clone(), unknown: 7 },
+                        Reply::LuserChannels { host: host.clone(), nick: nick.clone(), channels: 9999 },
+                        Reply::LuserMe { host: host.clone(), nick: nick.clone(), clients: 900, servers: 1 },
+                        Reply::LocalUsers { host: host.clone(), nick: nick.clone(), current: 845, max: 1000 },
+                        Reply::GlobalUsers { host: host.clone(), nick: nick.clone(), current: 9832, max: 23455 },
+                        Reply::StatsDLine { host: host.clone(), nick: nick.clone(), connections: 9998, clients: 9000, received: 99999 },
+        
+                        Reply::MotdStart { host: host.clone(), nick: nick.clone() },
+                        // TODO proper configurable MOTD
+                        Reply::Motd { host: host.clone(), nick: nick.clone(), line: "Foobar".to_string() },
+                        Reply::EndOfMotd { host: host.clone(), nick: nick.clone() }    
+                    ]
+                )
             },
             // these won't be passed down
             ClientToServerCommand::Unhandled { .. } => { },
@@ -81,5 +100,14 @@ pub fn run_server(
             ClientToServerCommand::Pong {} => { },
             ClientToServerCommand::Quit {} => { }
         };
+    }
+}
+
+fn send_replies(sender: &Sender<Reply>, replies: Vec<Reply>) {
+    for reply in replies {
+        if let Err(e) = sender.send(reply) {
+            println!("Error sending replies {:?}", e);
+            return;
+        }
     }
 }
