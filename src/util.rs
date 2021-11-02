7c015f875230546f535f9d4289fc6bc703e23a26
Vince Pergolizzi <pergolizzivincenzo@gmail.com>
2021-11-01 22:13:12 -0400

    regex

--- src/util.rs
+++ src/util.rs
@@ -3,17 +3,24 @@ use regex::Regex;
 // TODO work in progress, this will be used by the WHO command
 // to find users
 pub fn match_mask(input: &str, mask: &str) -> bool {
-    let re = match Regex::new(mask) {
+    let mut regex = mask.replace("*", ".*").replace("?", ".");
+    regex.push('$');
+
+    let re = match Regex::new(&regex) {
         Ok(re) => re,
         // TODO
         Err(e) => return false
     };
 
-    re.is_match(input)    
+    re.is_match(input)
 }
 
 #[test]
 fn foo() {
-    // <nick>!<username>@<host>
+    assert_eq!(false, match_mask("nick!username@host", "nick"));
+}
+
+#[test]
+fn baz() {
     assert_eq!(true, match_mask("nick!username@host", "nick*"));
 }
